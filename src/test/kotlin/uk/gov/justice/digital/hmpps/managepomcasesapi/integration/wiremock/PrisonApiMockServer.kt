64dbbc1e3118be3f177786732187cc2334328ecc
Ryan Kendall <ryankendallt@gmail.com>
2025-03-06 14:40:52 +0000

    added wiremock for prison api

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managepomcasesapi/integration/wiremock/PrisonApiMockServer.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.managepomcasesapi.integration.wiremock
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class PrisonApiMockServer : WireMockServer(8092) {
+  fun stubUncheckedPrisonerSearchResponse() {
+    stubFor(
+      get(urlPathMatching("/prisoner-search/prison/[A-Z]{3}"))
+        .willReturn(
+          aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("""{ "content": [] }"""),
+        ),
+    )
+  }
+}
+
+class PrisonApiExtension :
+  BeforeAllCallback,
+  AfterAllCallback,
+  BeforeEachCallback {
+  companion object {
+    @JvmField
+    val prisonApi = PrisonApiMockServer()
+  }
+
+  override fun beforeAll(context: ExtensionContext): Unit = prisonApi.start()
+  override fun beforeEach(context: ExtensionContext): Unit = prisonApi.resetAll()
+  override fun afterAll(context: ExtensionContext): Unit = prisonApi.stop()
+}
