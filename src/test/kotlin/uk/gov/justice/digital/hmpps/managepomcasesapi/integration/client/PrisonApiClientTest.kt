d9e9ac151605902089d3c6c0dddac1d612dd0562
Ryan Kendall <ryankendallt@gmail.com>
2025-03-26 15:26:33 +0000

    Removed StubbedRequests in favour of wiremock

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managepomcasesapi/integration/client/PrisonApiClientTest.kt
@@ -0,0 +1,45 @@
+package uk.gov.justice.digital.hmpps.managepomcasesapi.integration.client
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.managepomcasesapi.client.PrisonApiClient
+import uk.gov.justice.digital.hmpps.managepomcasesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.managepomcasesapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
+import uk.gov.justice.digital.hmpps.managepomcasesapi.integration.wiremock.PrisonApiExtension.Companion.prisonApi
+
+class PrisonApiClientTest : IntegrationTestBase() {
+  @Autowired
+  lateinit var prisonApiClient: PrisonApiClient
+
+  @BeforeEach
+  fun setup() {
+    hmppsAuth.stubGrantToken()
+  }
+
+  @Test
+  fun `Retrieving details for a given staff id`() {
+    prisonApi.stubStaffDetailResponse(
+      response = """{"staffId":"1234", "firstName":"Jane", "lastName":"Smith", "status": "ACTIVE"}"""
+        .trimIndent(),
+    )
+
+    val result = prisonApiClient.staffDetail(1234)
+
+    Assertions.assertThat(result).isNotNull
+    Assertions.assertThat(result?.staffId).isEqualTo(1234)
+    Assertions.assertThat(result?.firstName).isEqualTo("Jane")
+    Assertions.assertThat(result?.lastName).isEqualTo("Smith")
+    Assertions.assertThat(result?.status).isEqualTo("ACTIVE")
+  }
+
+  @Test
+  fun `Checking if a staff member has POM role in a prison`() {
+    prisonApi.stubHasPomRoleResponse(response = "true")
+
+    val result = prisonApiClient.hasPomRole(1234, "MDI")
+
+    Assertions.assertThat(result).isTrue
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/managepomcasesapi/integration/wiremock/PrisonerSearchApiMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/managepomcasesapi/integration/wiremock/PrisonerSearchApiMockServer.kt
@@ -3,14 +3,12 @@ package uk.gov.justice.digital.hmpps.managepomcasesapi.integration.wiremock
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
-import com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching
 import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
 class PrisonerSearchApiMockServer : WireMockServer(8092) {
-
   fun stubHealthPing(status: Int = 200) {
     stubFor(
       get("/prisoner-search-api/health/ping").willReturn(
@@ -22,15 +20,17 @@ class PrisonerSearchApiMockServer : WireMockServer(8092) {
     )
   }
 
-  fun stubUncheckedPrisonerSearchResponse() {
-    stubFor(
-      get(urlPathMatching("/prisoner-search/prison/[A-Z]{3}"))
-        .willReturn(
+  fun stubFindByPrisonIdResponses(vararg pageResponse: String, prisonCode: String? = "LEI") {
+    pageResponse.forEachIndexed { i, response ->
+      stubFor(
+        get("/prisoner-search-api/prisoner-search/prison/$prisonCode?page=$i&size=1500&include-restricted-patients=true").willReturn(
           aResponse()
             .withHeader("Content-Type", "application/json")
-            .withBody("""{ "content": [] }"""),
+            .withBody(response)
+            .withStatus(200),
         ),
-    )
+      )
+    }
   }
 }
 
